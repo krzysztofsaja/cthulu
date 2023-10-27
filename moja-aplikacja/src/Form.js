a718edac0b2b08eefad6465cf1c657195c1bb4c7
Zuzanna Rakowska <zuzanna.rakowska.afh@gmail.com>
2023-10-27 18:39:01 +0200

    Formularz dopasowany do karty - Brak dodawania zdj

--- moja-aplikacja/src/Form.js
+++ moja-aplikacja/src/Form.js
@@ -2,17 +2,29 @@ import { useForm } from "react-hook-form"
 
 export default function App() {
   const { register, handleSubmit } = useForm({
-    firstName: '',
-    lastName: '',
-    age:0,
+    defaultValues: {
+        firstName: '',
+        description: '',
+        age:0,
+        gender: '',
+        birthPlace: '',
+        residence: '',
+    }
   })
   const onSubmit = (data) => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+      <input {...register("description", { pattern: /^[A-Za-z]+$/i })} />
       <input type="number" {...register("age", { min: 18, max: 99 })} />
+      <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      <input {...register("birthPlace", { pattern: /^[A-Za-z]+$/i })} />
+      <input {...register("residence", { pattern: /^[A-Za-z]+$/i })} />
       <input type="submit" />
     </form>
   )
