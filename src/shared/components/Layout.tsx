fa8d0062f6a40cfe743929215fe45316f0e8ff8d
Krzysztof Saja <krzysztof.saja@usz.edu.pl>
2023-11-09 09:35:28 +0100

    Fixed render problem

--- src/shared/components/Layout.tsx
+++ src/shared/components/Layout.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="flex justify-between flex-col h-screen">{children}</div>
+  );
 };
 
 export default Layout;
