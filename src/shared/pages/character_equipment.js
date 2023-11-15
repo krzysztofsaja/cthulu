10b7a81401d4bcb71caed7b98b257106c4c2556e
Julia Wlodarczyk <k.julincia@gmail.com>
2023-11-15 21:28:05 +0100

    Szkielety stron aplikacji wraz z nawigacją

---
+++ src/shared/pages/character_equipment.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function CharacterEquipment() {
+  return (
+    <div>
+      <h1>Character Equipment</h1>
+      <Link href="/character_skill">Przejdź do umiejętności postaci</Link>
+    </div>
+  );
+}
+
+export default CharacterEquipment;
