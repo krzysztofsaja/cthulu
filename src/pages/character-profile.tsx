f79db84db43e167068d25091c65d198269381017
Zuzanna Rakowska <zuzanna.rakowska.afh@gmail.com>
2023-11-20 18:40:51 +0100

    Zrobiono merge, dodano Formularz do pliku z profil

--- src/pages/character-profile.tsx
+++ src/pages/character-profile.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Layout from "../shared/components/Layout";
+import CharacterForm from "./character-form";
 
 function CharacterProfile() {
   return (
     <Layout>
       <h1>Character</h1>
       <Link href="/character-equipment">Przejdź do ekwipunku postaci</Link>
+      <FormData></FormData>
     </Layout>
   );
 }
