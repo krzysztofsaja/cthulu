bbdc01c75e7dd04783b2e37d09caab798d907ce3
rakzuz <rakowska.zuzanna@wp.pl>
2023-11-21 10:42:59 +0100

    Formularz działa

--- src/pages/character-form.tsx
+++ src/pages/character-form.tsx
@@ -1,6 +1,6 @@
 import { data } from "autoprefixer";
 import React from "react";
-import { useForm, SubmitHandler, Controller } from "react-hook-form"
+import { useForm, handleSubmit, SubmitHandler, Controller } from "react-hook-form"
 
 type FormData = {
   firstName: string
@@ -12,7 +12,7 @@ type FormData = {
   picture: undefined
 }
 
-export default function App () {
+export default function CharacterForm () {
   const { register, setValue, handleSubmit, formState: { errors },} = useForm({
     defaultValues: {
         firstName: '',
@@ -27,22 +27,33 @@ export default function App () {
 // https://www.geeksforgeeks.org/how-to-upload-image-and-preview-it-using-reactjs/
 // https://www.youtube.com/watch?v=XlAs-Lid-TA
 
-  const onSubmit = handleSubmit((data) => console.log(data))
+  // const onSubmit = handleSubmit((data) => console.log(data))
 
-  // const [picture, setFile] = React.useState();
-  // const onSubmit = data => {
-  //   console.log(data);
-  //   console.log(data.target.files);
-  //     setFile(URL.createObjectURL(data.target.files[0]));
-  // }
+  const [picture, setFile] = React.useState();
+  const onSubmit = data => {
+    console.log(data);
+    console.log(data.file);
+      // setFile(URL.createObjectURL(data.file[0]));
+  }
+  // const onSubmit = async (data) => {
+  //   const formData = new FormData();
+  //   formData.append("file", data.file[0]);
+
+  //   const res = await fetch("http://localhost:5000/upload-file", {
+  //       method: "POST",
+  //       body: formData,
+  //   }).then((res) => res.json());
+  //   alert(JSON.stringify(`${res.message}, status: ${res.status}`));
+  // };
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h2>Set your Name:</h2>
       <input {...register("firstName", { required: true, maxLength: 20 })} />
       <h2>Describe your Character:</h2>
       <input {...register("description", { pattern: /^[A-Za-z]+$/i })} />
       <h2>Add Picture of your Character:</h2>
-      <input ref={register} type="file" name="picture"/>
+      {/* <input ref={register} type="file" name="picture"/> */}
+      <input type="file" {...register("picture")}/>
       <h2>Set age:</h2>
       <input type="number" {...register("age", { min: 18, max: 99 })} />
       <h2>Chose gender:</h2>
--- src/pages/character-profile.tsx
+++ src/pages/character-profile.tsx
@@ -2,12 +2,13 @@ import Link from 'next/link';
 import Layout from "../shared/components/Layout";
 import CharacterForm from "./character-form";
 
+
 function CharacterProfile() {
   return (
     <Layout>
       <h1>Character</h1>
       <Link href="/character-equipment">Przejdź do ekwipunku postaci</Link>
-      <FormData></FormData>
+      <CharacterForm></CharacterForm>
     </Layout>
   );
 }
