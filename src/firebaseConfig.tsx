fa4a952af3348eed417e8ad4f29cee4f9f928309
Zuzanna Rakowska <zuzanna.rakowska.afh@gmail.com>
2023-12-11 17:17:59 +0100

    Dodałam konfigurację mojego firebase'a

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,12 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+    apiKey: "AIzaSyBDW0tYPfqf5V95CgcAHQgyuhxX-oX5YnE",
+    authDomain: "call-of-cthulu-paprykarz.firebaseapp.com",
+    projectId: "call-of-cthulu-paprykarz",
+    storageBucket: "call-of-cthulu-paprykarz.appspot.com",
+    messagingSenderId: "194413661419",
+    appId: "1:194413661419:web:26d09a8a7941f0f2dd02ae",
+    measurementId: "G-NGZ50CNVS6"
 };
 
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
 export const database = getFirestore(app);
