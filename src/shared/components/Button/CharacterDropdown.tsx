a2f9a0e91f7e58631af62a690144d283bcdb0399
julwlodarczyk <127862439+julwlodarczyk@users.noreply.github.com>
2023-12-13 19:20:36 +0000

    Zmiana caharcterdropdown na komponent funkcyjny, zmiana stylowania na tailwind

--- src/shared/components/Button/CharacterDropdown.tsx
+++ src/shared/components/Button/CharacterDropdown.tsx
@@ -1,34 +1,52 @@
 // shared/components/Button/CharacterDropdown.tsx
-<<<<<<< HEAD
-import React, { useState } from 'react';
-=======
-import React from 'react';
-import dynamic from 'next/dynamic';
->>>>>>> refs/remotes/origin/character-button
-import Button from './Button'; // Adjust the path based on your project structure
+import React, { useReducer } from 'react';
+import Button from './Button'; // Dostosuj ścieżkę zgodnie z strukturą projektu
 
 interface CharacterDropdownProps {
   characters: string[];
 }
 
-<<<<<<< HEAD
+interface CharacterDropdownState {
+  selectedCharacter: string | null;
+  isDropdownOpen: boolean;
+}
+
+type CharacterDropdownAction =
+  | { type: 'TOGGLE_DROPDOWN' }
+  | { type: 'SELECT_CHARACTER'; character: string };
+
+const characterDropdownReducer = (
+  state: CharacterDropdownState,
+  action: CharacterDropdownAction
+): CharacterDropdownState => {
+  switch (action.type) {
+    case 'TOGGLE_DROPDOWN':
+      return { ...state, isDropdownOpen: !state.isDropdownOpen };
+    case 'SELECT_CHARACTER':
+      return { selectedCharacter: action.character, isDropdownOpen: false };
+    default:
+      return state;
+  }
+};
+
 const CharacterDropdown: React.FC<CharacterDropdownProps> = ({ characters }) => {
-  const [selectedCharacter, setSelectedCharacter] = useState<string | null>(null);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [state, dispatch] = useReducer(characterDropdownReducer, {
+    selectedCharacter: null,
+    isDropdownOpen: false,
+  });
 
   const toggleDropdown = () => {
-    setIsDropdownOpen((prevIsDropdownOpen) => !prevIsDropdownOpen);
+    dispatch({ type: 'TOGGLE_DROPDOWN' });
   };
 
   const handleCharacterSelect = (character: string) => {
-    setSelectedCharacter(character);
-    setIsDropdownOpen(false);
+    dispatch({ type: 'SELECT_CHARACTER', character });
   };
 
   return (
     <div className="relative inline-block text-center">
-      <Button onClick={toggleDropdown} label={selectedCharacter ? selectedCharacter : 'Select Character'} />
-      {isDropdownOpen && (
+      <Button onClick={toggleDropdown} label={state.selectedCharacter ? state.selectedCharacter : 'Select Character'} />
+      {state.isDropdownOpen && (
         <div className="origin-top-right absolute right-0 mt-2 w-328px rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             {characters.map((character, index) => (
@@ -46,68 +64,5 @@ const CharacterDropdown: React.FC<CharacterDropdownProps> = ({ characters }) => 
     </div>
   );
 };
-=======
-class CharacterDropdown extends React.Component<
-  CharacterDropdownProps,
-  { selectedCharacter: string | null; isDropdownOpen: boolean }
-> {
-  constructor(props: CharacterDropdownProps) {
-    super(props);
-    this.state = {
-      selectedCharacter: null,
-      isDropdownOpen: false,
-    };
-  }
-
-  toggleDropdown = () => {
-    this.setState((prevState) => ({
-      isDropdownOpen: !prevState.isDropdownOpen,
-    }));
-  };
-
-  handleCharacterSelect = (character: string) => {
-    this.setState({
-      selectedCharacter: character,
-      isDropdownOpen: false,
-    });
-  };
-
-  render() {
-    const { characters } = this.props;
-    const { selectedCharacter, isDropdownOpen } = this.state;
-
-    return (
-      <div className="relative inline-block text-center">
-        <Button onClick={this.toggleDropdown} label={selectedCharacter ? selectedCharacter : 'Select Character'} />
-        {isDropdownOpen && (
-          <div className="origin-top-right absolute right-0 mt-2 w-328px rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-              {characters.map((character, index) => (
-                <div key={character} className={index !== 0 ? 'border-t border-gray-300' : ''}>
-                  <Button
-                    onClick={() => this.handleCharacterSelect(character)}
-                    label={`[${character}]`}
-                    style={{
-                      width: '328px',
-                      height: '68px',
-                      fontFamily: 'Lexend',
-                      fontSize: '17px',
-                      fontWeight: 500,
-                      lineHeight: '21px',
-                      letterSpacing: '0em',
-                      textAlign: 'center',
-                    }}
-                    className="bg-blue-500 hover:bg-blue-700 text-white mx-auto my-auto p-6 rounded-md"
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
->>>>>>> refs/remotes/origin/character-button
 
 export default CharacterDropdown;
