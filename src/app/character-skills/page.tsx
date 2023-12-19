5890d126a4bd6ff4adb9ffc8cd9474d3ed5de3dc
julwlodarczyk <127862439+julwlodarczyk@users.noreply.github.com>
2023-12-19 08:52:33 +0000

    Utworzenie SearchBar w character-skills

--- src/app/character-skills/page.tsx
+++ src/app/character-skills/page.tsx
@@ -1,10 +1,32 @@
-import Link from "next/link";
-import Layout from "../../shared/components/Layout";
+// Import necessary modules and components
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Layout from '../../shared/components/Layout';
+import SearchBar from 
 
 function CharacterSkill() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (value) => {
+    setSearchQuery(value);
+  };
+
+  const handleSearchSubmit = () => {
+    console.log('Searching for:', searchQuery);
+    // Add your search logic here
+  };
+
   return (
     <Layout>
       <h1>Character Skill</h1>
+
+      {/* Include the SearchBar component here */}
+      <SearchBar
+        searchQuery={searchQuery}
+        onSearchChange={handleSearchChange}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
       <Link href="/backstory">Przejdź do historii postaci</Link>
     </Layout>
   );
