023f8783fbf0d9032f246f2c5b04e35be2c3ceb9
rakzuz <rakowska.zuzanna@wp.pl>
2024-01-04 10:44:19 +0100

    Kolejna próba zainicjonowania Autoryzacji Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import IntroHeader from "@/src/shared/components/IntroHeader";
 import Layout from "@/src/shared/components/Layout";
 import IntroContent from "../shared/components/IntroContent";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function Home() {
+  const auth = getAuth();
+
   return (
     <Layout>
       <IntroHeader></IntroHeader>
+      <button onClick={() => signOut(auth)}>Sing out of Firebase</button>
       <IntroContent></IntroContent>
     </Layout>
   );
--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,25 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBDW0tYPfqf5V95CgcAHQgyuhxX-oX5YnE",
-    authDomain: "call-of-cthulu-paprykarz.firebaseapp.com",
-    projectId: "call-of-cthulu-paprykarz",
-    storageBucket: "call-of-cthulu-paprykarz.appspot.com",
-    messagingSenderId: "194413661419",
-    appId: "1:194413661419:web:26d09a8a7941f0f2dd02ae",
-    measurementId: "G-NGZ50CNVS6"
+export const config = {
+    firebaseConfig:{
+        apiKey: "AIzaSyBDW0tYPfqf5V95CgcAHQgyuhxX-oX5YnE",
+        authDomain: "call-of-cthulu-paprykarz.firebaseapp.com",
+        projectId: "call-of-cthulu-paprykarz",
+        storageBucket: "call-of-cthulu-paprykarz.appspot.com",
+        messagingSenderId: "194413661419",
+        appId: "1:194413661419:web:26d09a8a7941f0f2dd02ae",
+        measurementId: "G-NGZ50CNVS6"
+    }
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const storage = getStorage(app);
-export const database = getFirestore(app);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
