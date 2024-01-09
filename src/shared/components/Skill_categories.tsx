9c2df38723e4e9b9f7e864d158da9401798d0a6f
julwlodarczyk <127862439+julwlodarczyk@users.noreply.github.com>
2024-01-09 23:38:00 +0000

    Dodanie Skill_categories

---
+++ src/shared/components/Skill_categories.tsx
@@ -0,0 +1,58 @@
+// Skill_categories.tsx
+import React from 'react';
+
+interface CategoryProps {
+  name: string;
+  onClick: () => void;
+}
+
+const Category: React.FC<CategoryProps> = ({ name, onClick }) => {
+  return (
+    <div
+      className="cursor-pointer bg-white text-black w-84 h-26 p-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105 text-xs"
+      onClick={onClick}
+      style={{
+        borderRadius: '5px', // Dostosuj stopień zaokrąglenia tutaj
+      }}
+    >
+      {name}
+    </div>
+  );
+};
+
+const SkillCategories: React.FC = () => {
+  const handleCategoryClick = (category: string) => {
+    console.log(`Category clicked: ${category}`);
+  };
+
+  const categories = [
+    { name: 'Accounting', percentage: '05%' },
+    { name: 'Anthropology', percentage: '01%' },
+    { name: 'Appraide', percentage: '05%' },
+    { name: 'Archeology', percentage: '01%' },
+  ];
+
+  return (
+    <div>
+      <div className="flex gap-1 mt-2">
+        {categories.map((category, index) => (
+          <Category
+            key={index}
+            name={category.name}
+            onClick={() => handleCategoryClick(category.name)}
+          />
+        ))}
+      </div>
+      <ul className="mt-2">
+        {categories.map((category, index) => (
+          <li key={index} className="flex items-center mb-2">
+            <div className="w-3 h-3 bg-white border border-gray-500 mr-1"></div>
+            {`${category.name} (${category.percentage})`}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SkillCategories;
