c99ecc6a4fcf103a56e6a05dbf894f69bd56b80c
Fyredale <226877@stud.usz.edu.pl>
2024-01-18 08:42:22 +0100

    dodanie favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,16 +11,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
+  <html lang="en">
+<head>
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff">
-      </head>
-      <body>{children}</body>
-    </html>
+</head>
+  <body>{children}</body>
+  </html>
   );
 }
